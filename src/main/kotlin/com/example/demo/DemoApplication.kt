0339a359df7997f3a5f40989fcf05f26c3885f54
Dominik Meister <dominik.meister@helsana.ch>
2020-09-22 15:19:28 +0200

    add sample data on bootup

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,10 +1,24 @@
 package com.example.demo
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+
+	@Bean
+	fun init(repository: PersonRepo) = CommandLineRunner {
+
+		val p = Person().apply {
+			firstName = "Hans"
+			lastName = "Muster"
+		}
+
+		repository.save(p)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
